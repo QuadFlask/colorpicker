e5a46812d7ce5c4f3127dac1e5bdf0f12131807a
QuadFlask <pop9310@gmail.com>
2015-05-10 12:50:55 +0900

    change some style

--- library/src/main/java/com/flask/colorpicker/CircleColorDrawable.java
+++ library/src/main/java/com/flask/colorpicker/CircleColorDrawable.java
@@ -9,10 +9,10 @@ import android.graphics.drawable.ColorDrawable;
 import com.flask.colorpicker.builder.PaintBuilder;
 
 public class CircleColorDrawable extends ColorDrawable {
-	private float strokeWidth = 3f;
-	private Paint strokePaint = PaintBuilder.newPaint().style(Paint.Style.STROKE).stroke(strokeWidth).color(0xff000000).build();
+	private float strokeWidth;
+	private Paint strokePaint = PaintBuilder.newPaint().style(Paint.Style.STROKE).stroke(strokeWidth).color(0xffffffff).build();
 	private Paint fillPaint = PaintBuilder.newPaint().style(Paint.Style.FILL).color(0).build();
-	private Paint fillBackPaint = PaintBuilder.newPaint().shader(PaintBuilder.createAlphaPatternShader((int) strokeWidth * 2)).build();
+	private Paint fillBackPaint = PaintBuilder.newPaint().shader(PaintBuilder.createAlphaPatternShader(16)).build();
 
 	public CircleColorDrawable() {
 		super();
@@ -28,7 +28,7 @@ public class CircleColorDrawable extends ColorDrawable {
 
 		int width = canvas.getWidth();
 		float radius = width / 2f;
-		strokeWidth = radius / 8f;
+		strokeWidth = radius / 12f;
 
 		this.strokePaint.setStrokeWidth(strokeWidth);
 		this.fillPaint.setColor(getColor());
