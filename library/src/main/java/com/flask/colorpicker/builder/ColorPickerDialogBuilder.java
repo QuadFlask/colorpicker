3f7a18f654819436d6fbff5c9fd1e97d598cfc1f
flask <QuadFlask>
2015-10-16 20:08:30 +0900

    Update support lib version, change dependency of AlertDialog to v7

--- library/src/main/java/com/flask/colorpicker/builder/ColorPickerDialogBuilder.java
+++ library/src/main/java/com/flask/colorpicker/builder/ColorPickerDialogBuilder.java
@@ -1,6 +1,6 @@
 package com.flask.colorpicker.builder;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
