40d32881bb4acfc830b916bf24b17b25f4ba8e9a
QuadFlask <pop9310@gmail.com>
2016-09-03 12:10:19 +0900

    Update sample usage; get selected color from view mode

--- app/src/main/java/com/flask/colorpicker/sample/SampleActivity2.java
+++ app/src/main/java/com/flask/colorpicker/sample/SampleActivity2.java
@@ -2,6 +2,11 @@ package com.flask.colorpicker.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
 
 public class SampleActivity2 extends ActionBarActivity {
 
@@ -9,6 +14,17 @@ public class SampleActivity2 extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sample2);
+
+		ColorPickerView colorPickerView = (ColorPickerView) findViewById(R.id.color_picker_view);
+		colorPickerView.addOnColorSelectedListener(new OnColorSelectedListener() {
+			@Override
+			public void onColorSelected(int selectedColor) {
+				Toast.makeText(
+						SampleActivity2.this,
+						"selectedColor: " + Integer.toHexString(selectedColor).toUpperCase(),
+						Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 }
